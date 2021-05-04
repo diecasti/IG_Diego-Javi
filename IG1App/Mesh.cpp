58d69b518f3e0553dd88da85235819077765037d
diecasti <diecasti@ucm.es>
2021-05-05 01:32:41 +0200

    antonio te corot los webos

--- IG1App/Mesh.cpp
+++ IG1App/Mesh.cpp
@@ -298,3 +298,22 @@ Mesh* Mesh::generaContCuboTexCor(GLdouble nl) {
 	return mesh;
 }
 
+//-------------------------------------------------------------------------
+void IndexMesh::render() const {
+	//… // Comandos OpenGL para enviar datos de arrays a GPU
+	// Nuevos comandos para la tabla de índices
+	if (vIndices != nullptr) {
+		glEnableClientState(GL_INDEX_ARRAY);
+		glIndexPointer(GL_UNSIGNED_INT, 0, vIndices);
+	}
+	//… // Comandos OpenGL para deshabilitar datos enviados
+	// Nuevo comando para la tabla de índices:
+	glDisableClientState(GL_INDEX_ARRAY);
+}
+//-------------------------------------------------------------------------
+
+// Comando para renderizar la malla indexada enviada
+void IndexMesh::draw() const {
+	glDrawElements(mPrimitive, nNumIndices,
+		GL_UNSIGNED_INT, vIndices);
+}
