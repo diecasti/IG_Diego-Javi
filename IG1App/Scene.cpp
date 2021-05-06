9c9aad187d3365b9beea20d119404abdeb84f82d
javimu06 <javimu06@ucm.es>
2021-05-06 02:07:02 +0200

    Escenas chopeadas, queda limpiar proyecto

--- IG1App/Scene.cpp
+++ IG1App/Scene.cpp
@@ -135,6 +135,18 @@ void Scene::changeScene(int id) {
 		else if (mId == 2) {
 			scene2();
 		}
+		else if (mId == 3) {
+			scene3();
+		}
+		else if (mId == 4) {
+			scene4();
+		}
+		else if (mId == 5) {
+			scene5();
+		}
+		else if (mId == 6) {
+			scene6();
+		}
 	}
 }
 //-------------------------------------------------------------------------
@@ -165,7 +177,7 @@ void Scene::setGL(int id)
 		glClearColor(0.0, 0.0, 0.0, 1.0);  // background color (alpha=1 -> opaque)
 	else if (id == 1)
 		glClearColor(1.0, 1.0, 1.0, 1.0);  // background color (alpha=1 -> opaque)
-	else if (id == 2)
+	else if (id == 2 || id == 3 || id == 4 || id == 5 || id == 6)
 		glClearColor(0.7, 0.8, 0.9, 1.0);
 	glEnable(GL_DEPTH_TEST);  // enable Depth test 
 	//Textures
@@ -327,4 +339,112 @@ void Scene::scene2() {
 	*/
 	/*
 	*/
+}
+
+void Scene::scene3(){
+	//TEXTURAS
+	Texture* noche = new Texture();
+	noche->load("..\\Bmps\\noche.bmp", 200);
+	gTextures.push_back(noche);
+
+	//EJES RGB
+	gObjects.push_back(new EjesRGB(400.0));
+
+	//NAVE
+	gObjects.push_back(new Sphere(100, 50, 50));
+
+	//VENTANA
+	gObjects.push_back(new Cylinder(80, 80, 50, 50, 50));
+	gObjects.back()->setModelMat(glm::translate(gObjects.back()->modelMat(), dvec3(60, 0, 0)));
+	gObjects.back()->setModelMat(glm::rotate(gObjects.back()->modelMat(), radians(90.0), dvec3(0, 1, 0)));
+
+	//CRISTALVENTANA
+	gObjects.push_back(new Disk(0, 80, 50, 50));
+	gObjects.back()->setModelMat(glm::translate(gObjects.back()->modelMat(), dvec3(110, 0, 0)));
+	gObjects.back()->setModelMat(glm::rotate(gObjects.back()->modelMat(), radians(90.0), dvec3(0, 1, 0)));
+
+	//UNION ALAS
+	gObjects.push_back(new Cylinder(20, 20, 400, 50, 50));
+	gObjects.back()->setModelMat(glm::translate(gObjects.back()->modelMat(), dvec3(0, 0, -200)));
+
+	//ALAS
+	gObjectsTranslucid.push_back(new DiskText(0, 300, 6, 6));
+	gObjectsTranslucid.back()->setModelMat(glm::translate(gObjectsTranslucid.back()->modelMat(), dvec3(0, 0, -200)));
+	gObjectsTranslucid.back()->setModelMat(glm::rotate(gObjectsTranslucid.back()->modelMat(), radians(30.0), dvec3(0, 0, 1)));
+	gObjectsTranslucid.back()->setTexture(noche);
+
+	gObjectsTranslucid.push_back(new DiskText(0, 300, 6, 6));
+	gObjectsTranslucid.back()->setModelMat(glm::translate(gObjectsTranslucid.back()->modelMat(), dvec3(0, 0, 200)));
+	gObjectsTranslucid.back()->setModelMat(glm::rotate(gObjectsTranslucid.back()->modelMat(), radians(30.0), dvec3(0, 0, 1)));
+	gObjectsTranslucid.back()->setTexture(noche);
+
+}
+
+void Scene::scene4() {
+	//EJES RGB
+	gObjects.push_back(new EjesRGB(400.0));
+
+	//ANILLO MULTICOLOR
+	gObjects.push_back(new AnilloCuadrado());
+}
+void Scene::scene5() {
+	//EJES RGB
+	gObjects.push_back(new EjesRGB(400.0));
+
+	//CUBO VERDE
+	gObjects.push_back(new Cubo(100));
+}
+void Scene::scene6(){
+	//TEXTURAS
+	Texture* noche = new Texture();
+	noche->load("..\\Bmps\\noche.bmp", 200);
+	gTextures.push_back(noche);
+
+	//EJES RGB
+	gObjects.push_back(new EjesRGB(400.0));
+
+	//CREACION DE CUERPO COMPUESTO
+	auto ce = new CompoundEntity();
+	gObjects.push_back(ce);
+
+	//NAVE
+	auto esfera = new Sphere(100, 50, 50);
+	ce->addEntity(esfera);
+
+	//VENTANA
+	auto escotilla = new CompoundEntity();
+	auto cilindro = new Cylinder(80, 80, 50, 50, 50);
+	cilindro->setModelMat(glm::rotate(cilindro->modelMat(), radians(90.0), dvec3(0, 1, 0)));
+	escotilla->addEntity(cilindro);
+
+	auto tapa = new Disk(0, 80, 50, 50);
+	tapa->setModelMat(glm::translate(tapa->modelMat(), dvec3(50, 0, 0)));
+	tapa->setModelMat(glm::rotate(tapa->modelMat(), radians(90.0), dvec3(0, 1, 0)));
+	escotilla->addEntity(tapa);
+
+	//mover la escotilla a su posicion
+	escotilla->setModelMat(glm::translate(escotilla->modelMat(), dvec3(60, 0, 0)));
+	ce->addEntity(escotilla);
+
+	//UNION ALAS
+	auto cilindro_Centro = new Cylinder(20, 20, 400, 50, 50);
+	cilindro_Centro->setModelMat(glm::translate(cilindro_Centro->modelMat(), dvec3(0, 0, -200)));
+	ce->addEntity(cilindro_Centro);
+
+	//ALAS
+	auto ala_izquierda = new DiskText(0, 300, 6, 6);
+	ala_izquierda->setModelMat(glm::translate(ala_izquierda->modelMat(), dvec3(0, 0, -200)));
+	ala_izquierda->setModelMat(glm::rotate(ala_izquierda->modelMat(), radians(30.0), dvec3(0, 0, 1)));
+	ala_izquierda->setTexture(noche);
+	ce->addEntityTranslucid(ala_izquierda);
+
+	auto ala_derecha = new DiskText(0, 300, 6, 6);
+	ala_derecha->setModelMat(glm::translate(ala_derecha->modelMat(), dvec3(0, 0, 200)));
+	ala_derecha->setModelMat(glm::rotate(ala_derecha->modelMat(), radians(30.0), dvec3(0, 0, 1)));
+	ala_derecha->setTexture(noche);
+	ce->addEntityTranslucid(ala_derecha);
+
+	//TRASLACION OBJETO COMPUESTO
+	ce->setModelMat(glm::translate(ce->modelMat(), dvec3(0, 200, 0)));
+	//ce->setModelMat(glm::translate(ce->modelMat(), dvec3(0, 0, 200)));
 }
