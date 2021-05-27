64c3ba0322c6613b765eef4ed99c5c40db762144
diecasti <diecasti@ucm.es>
2021-05-28 01:37:58 +0200

    Light Class

---
+++ IG1App/Light.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <GL/freeglut.h>
+#include <glm.hpp>
+
+#include "Camera.h"
+#include "Entity.h"
+#include "Texture.h"
+
+#include <vector>
+class Light { // Abstract class
+protected:
+	static GLuint cont; // Atributo para poder generar un nuevo id cada vez
+	GLuint id = GL_LIGHT0 + GL_MAX_LIGHTS; // Primer id no válido
+	// Atributos lumínicos y geométrico de la fuente de luz
+	glm::fvec4 ambient = { 0.1, 0.1, 0.1, 1 };
+	glm::fvec4 diffuse = { 0.5, 0.5, 0.5, 1 };
+	glm::fvec4 specular = { 0.5, 0.5, 0.5, 1 };
+	glm::fvec4 posDir = { 0, 0, 1, 0 };
+public:
+	// Añade setter’s para cambiar el valor de los atributos lumínicos
+	void setAmb(glm::fvec4 value);
+	void setDiffuse(glm::fvec4 value);
+	void setSpecular(glm::fvec4 value);
+	void setPosDir(glm::fvec4 value);
+	Light();
+	virtual ~Light() { disable(); }
+	 void uploadL() const;
+	// Método abstracto
+	virtual void upload(glm::dmat4 const& modelViewMat) const = 0;
+	void disable();
+	void enable();
+	//…
+};
+
+class DirLight : public Light {
+public:
+	virtual void upload(glm::dmat4 const& modelViewMat) const;
+	void setPosDir(glm::fvec3 dir);
+};
+
+
+class PosLight : public Light {
+protected:
+	// Factores de atenuación
+	GLfloat kc = 1, kl = 0, kq = 0;
+public:
+	virtual void upload(glm::dmat4 const& modelViewMat) const;
+	void setPosDir(glm::fvec3 dir);
+	void setAtte(GLfloat kca, GLfloat kla, GLfloat kqa) { kc = kca; kl = kla; kq = kqa; };
+};
+
+class SpotLight : public PosLight {
+protected:
+	// Atributos del foco
+	glm::fvec4 direction = { 0, 0, -1, 0 };
+	GLfloat cutoff = 180;
+	GLfloat exp = 0;
+public:
+	SpotLight(glm::fvec3 pos = { 0, 0, 0 })
+		: PosLight() {
+		posDir = glm::fvec4(pos, 1.0);
+	};
+	virtual void upload(glm::dmat4 const& modelViewMat) const;
+	void setSpot(glm::fvec3 dir, GLfloat cf, GLfloat e);
+};
